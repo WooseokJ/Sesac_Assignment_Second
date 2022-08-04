bdb094bf216f17013ee890c30ccbfb39212f24d1
wooseokJ <55547933+WooseokJ@users.noreply.github.com>
2022-08-04 12:09:33 +0900

    페이지네이션 기능 구현

--- NetworkBasic/NetworkBasic/Search/ImageSearchViewController.swift
+++ NetworkBasic/NetworkBasic/Search/ImageSearchViewController.swift
@@ -5,46 +5,45 @@ import Alamofire
 import SwiftyJSON
 import Kingfisher
 class ImageSearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
-    
-    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
     var imageArray : [String] = []
-    
+    var startpage = 1
+    var total = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self // 서비차와 서버통신할떄 해줌
         collectionView.delegate = self
         collectionView.dataSource = self
-        fetchImage()
+        collectionView.prefetchDataSource = self
         layoutSetting()
     }
     
     
     // fetchImage, requestImage, getImage, callRequestImage ...etc response에 따라 네이밍설명해주기도함.
-    func fetchImage() {
-        let text = "과자".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+    func fetchImage(query: String) {
+        let text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         let header : HTTPHeaders = ["X-Naver-Client-Id": APIKey.NAVER_ID, "X-Naver-Client-Secret": APIKey.NAVER_SECRET]
-        let url = EndPoint.imageSearchURL + "query=\(text)&display=30&start=1" // query와 start는 순서상관없음 ,  과자를 그냥넣으면 안됨. query= 내용은 과자를 UTF로 변환(%EA%B3%BC%EC%9E%90& 넣으면 나오긴함(네이버개발자센터 내용)
+        let url = EndPoint.imageSearchURL + "query=\(text)&display=30&start=\(startpage)" // query와 start는 순서상관없음 ,  과자를 그냥넣으면 안됨. query= 내용은 과자를 UTF로 변환(%EA%B3%BC%EC%9E%90& 넣으면 나오긴함(네이버개발자센터 내용)     startpage 부터 30개 갖고와라!
         AF.request(url, method: .get, headers: header).validate(statusCode: 200...500).responseJSON { [self] response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-//                print("JSON: \(json)")
+                print("JSON: \(json)")
+                total += json["total"].intValue
                 for item in json["items"].arrayValue{
-                    print(item)
+                    // 셀에서 url,uiimage 변환 vs 서버통신받는 시점에서 url,uimage변환  (전자가 더 좋음, 이미지용량이 너무크면 서버통신받는시점에서는 오랜시간거림
                     imageArray.append(item["link"].stringValue)
                 }
                 collectionView.reloadData()
             case .failure(let error):
                 print(error)
             }
-            
         }
-        
     }
-    
+    //MARK: 레이아웃잡기
     func layoutSetting() {
         let layout = UICollectionViewFlowLayout()
         let spacing : CGFloat = 8
@@ -67,15 +66,78 @@ class ImageSearchViewController: UIViewController, UICollectionViewDelegate, UIC
         
         let imageurl = URL(string: imageArray[indexPath.item])
         cell.gagaImageView.kf.setImage(with:imageurl)
-
-//        cell.gagaImageView.backgroundColor = .blue
         
         return cell
+    }
+    /*
+     페이지네이션 방법 3가지
+
+     1.will display cell (보여줄예정..) -> 실제로 그려주는애 (권장 X)
+
+     2.scrollview scroll 시점 활용  ( 가장많이사용, 보편적인방법) , 특정시점에 다다르면 하단이 더 보이게
+
+     3. protocol datasourceprefetching
+     */
+    
+    // 페이지네이션 방법1: 컬렉션뷰가 특정셀 그리려는 시점에 호출되는 메서드( 권장하는방법은 아님)
+    // 마지막 셀에 사용자가 위치해있는지 명확하게 확인하기 어려워
+    // func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    //  <#code#>
+    // }
+    
+    // 페이지네이션 방법2: UIScrollViewDelegateProtocol
+    // 테이블뷰,컬렉션뷰,스크롤뷰 상속받고있어서, 스크롤뷰 프로토콜을 사용할수있다.
+    // func scrollViewDidScroll(_ scrollView: UIScrollView) { // 스크롤 될떄마다 동작
+    //    print(scrollView.contentOffset) //얼마만큼 사용자가 내려왓는지 확인이 가능
+    // }
+}
+
+// 페이지네이션 방법3 : 용량큰 이미지 다운받아 셀 보여주는경우에 효과적 .
+// 셀이 화면에 보이기 전에 미리 필요한 리소스를 다운받을수있고, 필요하지 않다면 데이터를 취소할수있음.
+// ios10 이상부터 사용가능 스크롤 성능항상됨.
+extension ImageSearchViewController: UICollectionViewDataSourcePrefetching {
+    // 셀이 화면에 보이기 직전에 필요한 리소스를 미리 다운받는기능
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        //indexPaths는 배열, 0부터 시작
+        for index in indexPaths{
+            if (imageArray.count - 1 == index.item) && (imageArray.count < total){
+                startpage += 30
+                fetchImage(query: searchBar.text!)
+            }
+        }
+        
+        print("==========\(indexPaths)")
+    }
+    //취소(엄청빠르게 사용자가 스크롤할떄)
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        print("취소취소취소취소\(indexPaths)")
     }
     
 }
 
 
-
+extension ImageSearchViewController : UISearchBarDelegate {
+    //검색 버튼 클릭시 실행
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let text = searchBar.text {
+            imageArray.removeAll()
+            startpage = 1
+//            collectionView.scrollToItem(at: <#T##IndexPath#>, at: <#T##UICollectionView.ScrollPosition#>, animated: <#T##Bool#>)
+            fetchImage(query: text)
+        }
+    }
+    
+    //취소버튼눌를떄 실행
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        imageArray.removeAll()
+        collectionView.reloadData()
+        searchBar.text = ""
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+    //서치바에서 커버가 깜빡이기 시작할떄 실행
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+}
 
 
--- NetworkBasic/NetworkBasic/Search/SearchViewController.swift
+++ NetworkBasic/NetworkBasic/Search/SearchViewController.swift
@@ -2,31 +2,27 @@ import UIKit
 
 import Alamofire
 import SwiftyJSON
+import JGProgressHUD
 /**
- 
  프로토콜 :
  - delegate
  - datasource
- 
  1, 왼팔delegate/오른팔datasource
  2. 테이블뷰 아웃렛 연결
  3. 1번 2번 합친거
  */
 
-
-/*
- 
- */
-
 class SearchViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var searchBar: UISearchBar!
-    //    @IBOutlet weak var third: UITableView!
-//    @IBOutlet weak var second: UITableView!
     @IBOutlet weak var searchtableView: UITableView!
     
     //boxoffice 담을 배열
     var list: [BoxOfficeModel] = []
+    let hud = JGProgressHUD()
+
+ 
+    
     var inputDate : String {
         get{
             //내풀이
@@ -45,10 +41,6 @@ class SearchViewController: UIViewController,UITableViewDelegate, UITableViewDat
             // 네트워크 느린환경에대한 테스트도 중요 : 실제기기로 테스트시 confition조절가능,
             // 시뮬레이션도 가능하지만 추가설치필요(권장하진않음)
         }
-        set{
-            
-        }
-        
     }
     // 타입 어노테이션 vs 타입추론 -> 누가더빠른가?   과거엔 어노테이션이 빨랏지만 요즘엔 추론이더빠름 (하지만 큰차이없음.)
     // what's new in swift
@@ -56,6 +48,8 @@ class SearchViewController: UIViewController,UITableViewDelegate, UITableViewDat
     var username = "" //타입추론
     
     override func viewDidLoad() {
+
+        
         super.viewDidLoad()
         //연결고리 작업 : 테이블뷰가 해야할 역할 > 뷰컨트롤러에게 요청
         searchtableView.delegate = self
@@ -63,31 +57,35 @@ class SearchViewController: UIViewController,UITableViewDelegate, UITableViewDat
         // 테이블뷰 사용할 테이블뷰 셀(XIB)등록
         //xib: xml interface builder (예전엔 NIB이라불림)
         searchtableView.register(UINib(nibName: ListTableViewCell.reuseIdentifier, bundle: nil), forCellReuseIdentifier: ListTableViewCell.identifier)
-        
         requestBoxOffice(text: inputDate)
         searchBar.delegate = self
-        
     }
     
+    
+    
     func requestBoxOffice(text: String) {
+        
+        hud.textLabel.text = "Loading"
+        hud.show(in: view)
         list.removeAll()
+        
         //인증키 제한
         let url = "\(EndPoint.boxOfficeURL)key=\(APIKey.BOXOFICE)&targetDt=\(text)"
-        AF.request(url, method: .get).validate().responseJSON { [self] response in
+        //경고창 제거방법 두가지 1. alamofire+ codable, 2. alamofire + response(json,data,string)를알맞은걸로바꾸기
+        AF.request(url, method: .get).validate().responseData { [self] response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-//                print("JSON: \(json)")
+                print("JSON: \(json)")
                 //                let movieNm1 = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"].stringValue
                 //                let movieNm2 = json["boxOfficeResult"]["dailyBoxOfficeList"][1]["movieNm"].stringValue
-                //                let movieNm3 = json["boxOfficeResult"]["dailyBoxOfficeList"][2]["movieNm"].stringValue
                 //                //배열 리스트 추가
                 //                ls.append(movieNm1)
                 //                ls.append(movieNm2)
-                //                ls.append(movieNm3)
+
              
                 for item in json["boxOfficeResult"]["dailyBoxOfficeList"].arrayValue { //arrayValue하면 그 값의 배열원소 하나하나가 item이 된다.
-                    print(item)
+//                    print(item)
                     let movieNm = item["movieNm"].stringValue
                     let openDtt = item["openDt"].stringValue
                     let audiAcc = item["audiAcc"].stringValue
@@ -96,12 +94,14 @@ class SearchViewController: UIViewController,UITableViewDelegate, UITableViewDat
                 }
 
                 searchtableView.reloadData() //데이터가 모두 ls에 추가된뒤 갱신(reload)해줘야한다.
-
+                hud.dismiss(animated: true)
                 
             case .failure(let error):
+                hud.dismiss(animated: true)
                 print(error)
             }
         }
+        
     }
     
     // UIviewController에 있는게 아니잖아 그래서 override하면 오류남
