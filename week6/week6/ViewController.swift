8cc875802ce2487edf19113b535aad9bd60c83f7
wooseokJ <55547933+WooseokJ@users.noreply.github.com>
2022-08-08 09:27:56 +0900

    week6

---
+++ week6/week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by useok on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
